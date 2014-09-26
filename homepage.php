8ab2ce2d0d02290d9c3afb88b3395469163653f8
younges <elizasyoung@gmail.com>
2014-09-26 13:55:17 -0700

    Deleted Emilie and my testing stuff

--- homepage.php
+++ homepage.php
@@ -27,8 +27,7 @@ get_header(); ?>
   <div class="grid">
     <div class="whole">
     	<h3>Learn how to cook with basil</h3>
-    	<p>Kitchen N Things and Seattle restauranteur Tom Douglas' favorite summer recipes. Test!</p>
-    	<p>Emilie's test</p>
+    	<p>Kitchen N Things and Seattle restauranteur Tom Douglas' favorite summer recipes.</p>
   </div>
 </div>
 </section>
