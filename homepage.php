ae3ebc551384b88725999e233d019b372e5373b5
Emilie Thaler <emiliethaler@gmail.com>
2014-10-07 13:27:08 -0700

    A WHOLE BUNCHA SHIT

--- homepage.php
+++ homepage.php
@@ -1,93 +1,71 @@
 <?php
 
-/**
-Template Name: Home
+/*
+** Template Name: Home
 */
 ?>
 
+<?php get_header(); ?>
 
-<?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package kitchen
- */
+<div id="primary" class="content-area">
 
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<main id="main" class="site-main" role="main">
 
 
-<section class="demo">
-<div class="grid">
+<!-- Hero Unit -->
+<div class="hero-unit">
 	<div class="border-top"></div>
-
 	<?php if( get_field('hero_image') ): ?>
-	<a href="http://www.elizayoungdesign.com/kitchen-n-things/event/cooking-with-basil/" title="Cooking with Basil"><img src="<?php the_field('hero_image');?>" class="hero";/></a>
-      	<div class="border-bottom"></div>
+		<a href="http://www.elizayoungdesign.com/kitchen-n-things/event/cooking-with-basil/" title="Cooking with Basil"><img src="<?php the_field('hero_image');?>" class="hero" alt="<?php the_field('hero_image_title');?>"></a>
+    <?php endif; ?>
+    <div class="border-bottom"></div>
+</div><!-- End Hero Unit -->
 
-  </div>
-</div>
-  	<?php endif; ?>
-</section>
+<div class="row">
+	<div class="col-md-8 center-block">
 
-<section class="demo1">
-<div class="grid">
-      <div class="unit three-fifths">
-        <!--<h3>Upcoming Events</h3>-->
-        <h3><?php the_field('top_left_module_title'); ?></h3>
-        <?php get_calendar(); ?>
-      </div>
-      <div class="unit two-fifths">
-          <h3><?php the_field ('top_right_module_title'); ?></h3>
-      </div>
-    </div>
-</section>
+	</div>
+</div><!-- end grid -->
 
+<div class="row">
+	<!-- Upcoming Events -->
+	<div class="col-md-7">
+		<div class="red-background content-area">
+			<h3>Events</h3>
+			<?php get_calendar(); ?>
+		</div>
+	</div>
+	<!-- Featured Product -->
+	<div class="col-md-5">
+		<div class="green-background content-area">
+  			<h3>Featured Product</h3>
+  		</div>
+	</div>
+</div><!-- end grid -->
 
-<section class="demo2">
-
-	<div class="grid">
-		<div class="unit one-third otherstuff">
-			
-			<h3><?php the_field ('middle_left_module_title'); ?></h3>
-			<?php
-global $post;
-$args = array( 'posts_per_page' => 3 );
-$myposts = get_posts( $args );
-foreach( $myposts as $post ) :	setup_postdata($post); ?>
-<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a><br />
-<?php endforeach; ?>
+<div class="row">
+	<!-- Newest Recipe -->
+	<div class="col-md-4">
+		<h3>Newest Recipe</h3>
+		<?php
+			global $post;
+			$args = array( 'posts_per_page' => 3 );
+			$myposts = get_posts( $args );
+			foreach( $myposts as $post ) :	
+				setup_postdata($post); ?>
+			<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a><br />
+		<?php endforeach; ?>
+	</div>
 	
+	<!-- Other Title-->
+	<div class="col-md-4">
+			<h3>Title</h3>
 	</div>
-
-		<div class="unit one-third moreotherstuff">
-			<h3><?php the_field ('middle_middle_module_title'); ?></h3>
+	<div class="col-md-4">
+			<h3>Title</h3>
 	</div>
-		<div class="unit one-third moremoreotherstuff">
+</div><!-- end grid --></div>
 
-			<h3><?php the_field ('middle_right_module_title'); ?></h3>
-	</div>
-</div>
-</section>
-
-</div>
-
-
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</main><!-- #main -->
+</div><!-- #primary -->
 <?php get_footer(); ?>
