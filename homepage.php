a7f1b75fc2b05bf400efc807da7ba2e55c0d805f
younges <elizasyoung@gmail.com>
2014-10-16 13:55:36 -0700

    Created larger thumbnail pictures for new/staff

--- homepage.php
+++ homepage.php
@@ -34,12 +34,12 @@
 		<?php dynamic_sidebar('events_widget'); ?>
 		</div>
 	</div>
-	<!-- Featured Product -->
+	<!-- 101 Series -->
+
 	<div class="col-md-5">
-		<div class="green-background content-area">
-			  			<?php dynamic_sidebar('featured_widget'); ?>
-			  			  		<h3>1</h3>
-  		</div>
+		<div class="tan-background content-area">
+			<?php dynamic_sidebar('101_widget'); ?>
+		</div>
 	</div>
 </div><!-- end grid -->
 
@@ -52,19 +52,49 @@
 	</div>
 </div>
 	
-	<!-- Other Title-->
+	<!-- New to the Store-->
 	<div class="col-md-4">
 		<div class="orange-background content-area">
+			<h1>New to the Store</h1>
+			<?php if( get_field('new_items_image') ): ?>
+
+	<img src="<?php the_field('new_items_image'); ?>" />
+
+<?php endif; ?>
 			<?php dynamic_sidebar('new_product_widget'); ?>
 			<h3><font color= "#FBC468">1</font></h3>
 	</div>
 </div>
-	<div class="col-md-4">
-		<div class="tan-background content-area">
-			<?php dynamic_sidebar('101_widget'); ?>
-		</div>
+<!--Staff Picks-->
+<div class="col-md-4">
+		<div class="green-background content-area">
+			<h1> Staff Pick </h1>
+			<?php if( get_field('featured_product_image') ): ?>
+
+	<img src="<?php the_field('featured_product_image'); ?>" />
+
+<?php endif; ?>
+						<div class="creature_feature">
+			  			<?php dynamic_sidebar('featured_widget'); ?>
+			  		</div>
+			  			  		<h3>1</h3>
+  		</div>
 	</div>
 </div><!-- end grid --></div>
+
+<div class="row">
+	<!-- Upcoming Events -->
+	<div class="col-md-7">
+		<div class="red-background content-area">
+		</div>
+	</div>
+	<!-- Featured Product -->
+	<div class="col-md-5">
+		<div class="green-background content-area">
+			  			  		<h3>1</h3>
+  		</div>
+	</div>
+</div><!-- end grid -->
 
 </main><!-- #main -->
 </div><!-- #primary -->
